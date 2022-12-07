85563667fd86e8a544a98ba70cd097dbd4ea1e94
Safaruzzaman Shovo <118802492+SafaruzzamanShovo@users.noreply.github.com>
2022-12-08 01:37:18 +0600

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the ASCII Value : ");
+    scanf("%d",&n);
+    printf("Equivalent Character of the ASCII value is : %c\n",n);
+    return 0;
+}
