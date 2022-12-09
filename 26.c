d095cbe98f5a82d672c518125dcf486a70ea3422
Safaruzzaman Shovo <118802492+SafaruzzamanShovo@users.noreply.github.com>
2022-12-09 15:06:15 +0600

    Update 26.c

--- 26.c
+++ 26.c
@@ -8,25 +8,26 @@ int main()
     char ch;
     int i=0,lower,upper,digit,line,others,space;
     lower=upper=digit=others=space=0;
-    line=1;
-    fp = fopen("text.txt", "r");
+    line =0;
+    fp = fopen("shovo.txt", "r");
     if (fp == NULL){
         printf("File doesn't exist\n");
     }
     else{
-
-       ch=fgetc(fp);
+        ch=fgetc(fp);
        while(!feof(fp)){
+            //ch=fgetc(fp);
             if(ch<='z' && ch>='a') lower++;
             else if(ch<='Z' && ch>='A') upper++;
             else if(ch>='0' && ch<='9') digit++;
-            else if(ch==32) space++;
-            else if( ch== 10 ) line++;
+            else if(ch==10) line++;
+            else if(ch==' ') space++;
             else others++;
 
             ch=fgetc(fp);
+
         }
-        printf("Lowercase: %d \nuppercase: %d\ndigits: %d\nline: %d\nspace: %d\nspecial character: %d\n",lower,upper,digit, line, space, others);
+        printf("Lowercase: %d \n uppercase: %d\n digits: %d\n line: %d\n space: %d\n special character: %d\n",lower,upper,digit, line, space, others);
 
     }
 
